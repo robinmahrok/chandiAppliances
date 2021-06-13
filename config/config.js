52b7e7ff613108f459f71b0123b0b84a278eda33
robinmahrok <robinsinghmahrok@gmail.com>
2021-06-13 16:27:09 +0530

    changes

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // OAuth credentails common for mailer functionality
+    credentials: {
+        password: {
+            email:"chandiappliancesandservices@gmail.com",
+            password:"Chandi@123"
+        }
+      },
+
+    excludedRoutes: [
+        `/shorturl`,
+       `/loginRoutes`
+    ],
+
+    //serverUrl: ec2-3-84-149-36.compute-1.amazonaws.com
+
+
+};
